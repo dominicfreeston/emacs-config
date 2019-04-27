00549e9f354777234bac0d95e3fef8de606c6143
Dominic Freeston <dominic.freeston@gmail.com>
2019-04-27 15:45:31 +0100

    Extract out package setup

---
+++ my-packages.el
@@ -0,0 +1,20 @@
+;; Load all installed packages
+(require 'package)
+
+;; List of required packages
+(setq package-selected-packages
+  '(
+    magit
+    ))
+
+;; Setup archives and download packages
+(add-to-list 'package-archives
+             '("melpa-stable" . "https://melpa.org/packages/") t)
+
+(package-initialize)
+
+(when (not package-archive-contents)
+  (message "%s" "Emacs is now refreshing its package database...")
+  (package-refresh-contents))
+
+(package-install-selected-packages)
