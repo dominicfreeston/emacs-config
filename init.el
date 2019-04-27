e810d9bb83422ccb1b06e62d06ff77dcadb7cb7d
Dominic Freeston <dominic.freeston@gmail.com>
2019-04-27 14:42:45 +0100

    Split out config into separate file

--- init.el
+++ init.el
@@ -4,33 +4,6 @@
              '("melpa-stable" . "https://melpa.org/packages/") t)
 (package-initialize)
 
-;; Prettify
-(load-theme 'tango-dark)
-
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
-(global-visual-line-mode 1)
-
-(add-to-list 'default-frame-alist '(font . "Source Code Pro 14"))
-(add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
-(add-to-list 'default-frame-alist '(ns-appearance . dark))
-(add-to-list 'default-frame-alist '(fullscreen . maximized))
-
-(setq frame-title-format nil)
-(setq inhibit-startup-screen t)
-
-;; Sensible Defaults
-(setq ring-bell-function 'ignore)
-(setq mac-right-option-modifier nil)
-(fset 'yes-or-no-p 'y-or-n-p)
-(define-key global-map (kbd "RET") 'newline-and-indent)
-
-;; Backups and Autosaves
-(defvar --backup-directory (concat user-emacs-directory "backups"))
-(if (not (file-exists-p --backup-directory))
-        (make-directory --backup-directory t))
-(setq backup-directory-alist `(("." . ,--backup-directory)))
-(setq vc-make-backup-files t)
-
-;; Editing
-(electric-pair-mode 1)
+;; Load config
+(add-hook 'after-init-hook '(lambda ()
+  (load "~/.emacs.d/my-config.el")))
