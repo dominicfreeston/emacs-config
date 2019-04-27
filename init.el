57dd35e702e686b076d228c27669da87fc4a1449
Dominic Freeston <dominic.freeston@gmail.com>
2019-04-27 15:35:35 +0100

    Add magit package, hijacking selected-packages

--- init.el
+++ init.el
@@ -1,9 +1,24 @@
 ;; Load all installed packages
 (require 'package)
+
+;; List of required packages
+(setq package-selected-packages
+  '(
+    magit
+    ))
+
+;; Setup archives and download packages
 (add-to-list 'package-archives
              '("melpa-stable" . "https://melpa.org/packages/") t)
+
 (package-initialize)
+
+(when (not package-archive-contents)
+  (message "%s" "Emacs is now refreshing its package database...")
+  (package-refresh-contents))
+
+(package-install-selected-packages)
 
 ;; Load config
 (add-hook 'after-init-hook '(lambda ()
-  (load "~/.emacs.d/my-config.el")))
+			      (load (concat user-emacs-directory "my-config.el"))))
