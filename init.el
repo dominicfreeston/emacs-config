3114f8b7307a679ab122cb395afc75e8ff8683de
Dominic Freeston <dominic.freeston@gmail.com>
2019-04-28 09:26:29 +0100

    Add Magit key bindings everywhere

--- init.el
+++ init.el
@@ -5,6 +5,9 @@
 (when (memq window-system '(mac ns x))
   (exec-path-from-shell-initialize))
 
+(global-set-key (kbd "C-x g") 'magit-status)
+(global-set-key (kbd "C-x M-g") 'magit-dispatch-popup)
+
 ;; Load config
 (add-hook 'after-init-hook '(lambda ()
 			      (load (concat user-emacs-directory "my-config.el"))))
