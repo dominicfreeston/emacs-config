b0c908caa4c17267a5a4fc482def01eba4aa10cd
Dominic Freeston <dominic.freeston@gmail.com>
2019-04-30 17:10:06 +0100

    Setup smartparens key-bindings

--- my-packages.el
+++ my-packages.el
@@ -12,8 +12,44 @@
 
 (use-package smartparens-config
   :ensure smartparens
-  :config (smartparens-global-mode t))
+  :config
+  (smartparens-global-mode t))
 
 (use-package clojure-mode)
 
 (use-package cider)
+
+;; Key-maps
+
+(bind-keys
+ :map smartparens-mode-map
+ ("H-a" . sp-beginning-of-sexp)
+ ("H-e" . sp-end-of-sexp)
+ ("H-f" . sp-forward-sexp)
+ ("H-b" . sp-backward-sexp)
+
+ ("C-<down>" . sp-down-sexp)
+ ("C-<up>"   . sp-backward-up-sexp)
+ ("M-<down>" . sp-backward-down-sexp)
+ ("M-<up>"   . sp-up-sexp)
+
+ ("H-n" . sp-next-sexp)
+ ("H-p" . sp-previous-sexp)
+
+ ("C-S-f" . sp-forward-symbol)
+ ("C-S-b" . sp-backward-symbol)
+
+ ("C-<right>" . sp-forward-slurp-sexp)
+ ("C-<left>" . sp-forward-barf-sexp)
+ ("M-<left>"  . sp-backward-slurp-sexp)
+ ("M-<right>"  . sp-backward-barf-sexp)
+
+ ("C-M-t" . sp-transpose-sexp)
+ ("C-M-k" . sp-kill-sexp)
+ ("C-k"   . sp-kill-hybrid-sexp)
+ ("M-k"   . sp-backward-kill-sexp)
+ ("C-M-w" . sp-copy-sexp)
+ 
+ ("M-[" . sp-backward-unwrap-sexp)
+ ("M-]" . sp-unwrap-sexp)
+ ("M-p" . sp-splice-sexp))
