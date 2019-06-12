925a3158cfcefd8ed5951765e5528c3920089fb5
Dominic Freeston <dominic.freeston@gmail.com>
2019-06-12 11:14:15 +0100

    Setup projectile properly

--- my-packages.el
+++ my-packages.el
@@ -7,7 +7,12 @@
   (exec-path-from-shell-initialize))
 
 ;; Tools
-(use-package projectile)
+(use-package projectile
+  :config
+  (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
+  (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+  (projectile-mode +1))
+
 (use-package magit
   :bind
   ("C-x g" . magit-status))
