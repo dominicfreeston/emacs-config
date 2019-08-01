8bbfd9301816e0d4ff14d68565216a8fb4e93fe1
Dominic Freeston <dominic.freeston@gmail.com>
2019-08-01 21:21:40 +0100

    Add YASnippet, Company, clj-refactor

--- my-packages.el
+++ my-packages.el
@@ -1,29 +1,55 @@
 ;; Setup use-package
+;; https://github.com/jwiegley/use-package
 (require 'use-package)
 
+;; Make sure environment is same as shell on mac
 (use-package exec-path-from-shell
   :if (memq window-system '(mac ns))
   :config
   (exec-path-from-shell-initialize))
 
 ;; Tools
+;; PROJECTILE
 (use-package projectile
   :config
   (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
   (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
   (projectile-mode +1))
 
+;; MAGIT
 (use-package magit
   :bind
   ("C-x g" . magit-status)
   :config
   (setq magit-display-buffer-function 'magit-display-buffer-same-window-except-diff-v1))
 
+;; SMARTPARENS
 (use-package smartparens-config
   :ensure smartparens
   :config
   (smartparens-global-mode t)
   (show-smartparens-global-mode t))
+
+;; YASNIPPET
+;; (use-package yasnippet-snippets)
+(use-package yasnippet
+  :init
+  (yas-global-mode 1))
+
+;; COMPANY - better autocompletion
+;; http://company-mode.github.io/
+;; https://cider.readthedocs.io/en/latest/code_completion/
+;; M-n / M-p to scroll
+;; C-s to search and C-o to stop
+;; C-w to see source, F1 to see documentation
+(use-package company
+  :config
+  (global-company-mode)
+  (setq company-idle-delay 1)
+  (global-set-key (kbd "TAB") #'company-indent-or-complete-common))
+
+(add-hook 'cider-repl-mode-hook #'cider-company-enable-fuzzy-completion)
+(add-hook 'cider-mode-hook #'cider-company-enable-fuzzy-completion)
 
 ;; Languages
 (use-package fennel-mode
@@ -35,11 +61,19 @@
 (add-hook 'fennel-mode-hook #'smartparens-strict-mode)
 (setq fennel-mode-switch-to-repl-after-reload nil)
 
+(use-package clj-refactor)
+(defun my-clojure-mode-hook ()
+    (clj-refactor-mode 1)
+    (yas-minor-mode 1) ; for adding require/use/import statements
+    ;; This choice of keybinding leaves cider-macroexpand-1 unbound
+    (cljr-add-keybindings-with-prefix "C-c C-c"))
+
 (use-package clojure-mode)
+
 (add-hook 'clojure-mode-hook #'smartparens-strict-mode)
+(add-hook 'clojure-mode-hook #'my-clojure-mode-hook)
 
 (use-package cider)
-
 
 ;; Key-maps
 
