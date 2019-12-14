4d6021205d74997255fbd30d303a47839b4833ee
Dominic Freeston <dominic.freeston@gmail.com>
2019-12-14 14:32:34 +0000

    Rewrite config in org-mode

--- init.el
+++ init.el
@@ -1,14 +1,1 @@
-;; HACKAROUND for https://debbugs.gnu.org/cgi/bugreport.cgi?bug=34341
-(setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
-
-;; Redirect custom
-(setq custom-file (concat user-emacs-directory ".emacs-custom.el"))
-(load custom-file)
-
-;; Load packages
-(load (concat user-emacs-directory "my-packages-setup.el"))
-(load (concat user-emacs-directory "my-packages.el"))
-
-;; Load config
-(add-hook 'after-init-hook '(lambda ()
-			      (load (concat user-emacs-directory "my-config.el"))))
+(org-babel-load-file (concat user-emacs-directory "config.org"))
